3c49004bcfea14e06d326f18edce2758328553c3
tadashi-aikawa <syou.maman@gmail.com>
2017-08-14 22:34:02 +0900

    :hammer: Extract app name to constants

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import re
+
 from setuptools import setup
+
+APP_NAME = 'yourapp'
 
 requires = [
     'owlmixin',
@@ -11,8 +15,16 @@ extras = {
 }
 
 setup(
-    name='yourapp',
+    name=APP_NAME,
+    version=re.search(
+        r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
+        open(f'{APP_NAME}/__init__.py').read()).group(1),
     install_requires=requires,
     test_requires=extras['test'],
     extras_require=extras,
+    entry_points={
+        'console_scripts': [
+            f'{APP_NAME} = {APP_NAME}.main:main'
+        ],
+    },
 )
